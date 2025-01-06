5775aa2417e7581a7877ad7c55f9b194e9ff5aa6
St. Mark <adebayomarkadedayo@gmail.com>
2025-01-06 15:12:40 +0100

    code

---
+++ rxconfig.py
@@ -0,0 +1,136 @@
+# rxconfig.py
+
+# The name of your app.
+name = "E-Library"
+
+# The port to run the app on.
+port = 8501
+
+# The host to run the app on.
+host = "localhost"
+
+# The directory to store static assets.
+static_dir = "./assets"
+
+# The directory to output the built app.
+output_dir = "./build"
+
+# The database URL.
+db_url = "sqlite:///./db.sqlite"
+
+# The secret key for the app.
+secret_key = "your-secret-key"
+
+# The title of the app.
+title = "E-Library"
+
+# The description of the app.
+description = "A Reflex app"
+
+# The author of the app.
+author = "St. Mark"
+
+# The version of the app.
+version = "1.0.0"
+
+# The license of the app.
+license = "MIT"
+
+# The repository URL of the app.
+repository = "https://github.com/your-username/my-reflex-app"
+
+# The deployment target.
+deployment_target = "local"
+
+# The deployment configuration.
+deployment_config = {}
+
+# The custom components.
+custom_components = []
+
+# The custom scripts.
+custom_scripts = []
+
+# The custom styles.
+custom_styles = []
+
+# The custom settings.
+custom_settings = {}
+
+# The custom environment variables.
+custom_environment_variables = {}
+
+# The custom plugins.
+custom_plugins = []
+
+# The custom hooks.
+custom_hooks = []
+
+# The custom middleware.
+custom_middleware = []
+
+# The custom routes.
+custom_routes = []
+
+# The custom templates.
+custom_templates = []
+
+# The custom translations.
+custom_translations = []
+
+# The custom assets.
+custom_assets = []
+
+# The custom data.
+custom_data = {}
+
+# The custom functions.
+custom_functions = {}
+
+# The custom classes.
+custom_classes = {}
+
+# The custom modules.
+custom_modules = []
+
+# The custom packages.
+custom_packages = []
+
+# The custom dependencies.
+custom_dependencies = []
+
+# The custom requirements.
+custom_requirements = []
+
+# The custom build options.
+custom_build_options = {}
+
+# The custom run options.
+custom_run_options = {}
+
+# The custom deploy options.
+custom_deploy_options = {}
+
+# The custom export options.
+custom_export_options = {}
+
+# The custom init options.
+custom_init_options = {}
+
+# The custom login options.
+custom_login_options = {}
+
+# The custom logout options.
+custom_logout_options = {}
+
+# The custom db options.
+custom_db_options = {}
+
+# The custom script options.
+custom_script_options = {}
+
+# The custom cloud options.
+custom_cloud_options = {}
+
+# The custom component options.
+custom_component_options = {}
